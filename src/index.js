82324bbaa4b14264d911659740d7dedf0031351b
Michael Lei <michaelleie0ca@gmail.com>
2022-12-31 12:13:57 -0800

    Add nav bar switching functionality

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const pages = [home, menu, contact];
 const homeNav = document.getElementById("home");
 const menuNav = document.getElementById("menu");
 const contactNav = document.getElementById("contact");
-
-[homeNav, menuNav, contactNav].forEach((mItem, i) => {
-	mItem.addEventListener("click", renderPage(pages[i]));
-});
-const content = document.getElementById("content");
-
 const renderPage = (page) => {
 	content.innerHTML = page;
 };
+[homeNav, menuNav, contactNav].forEach((menuItem, i) => {
+	menuItem.addEventListener("click", () => {
+		renderPage(pages[i]);
+	});
+});
+renderPage(home);
